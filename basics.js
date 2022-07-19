744c25c52c1cb8423d4aaca39aa9760a32baf2e1
Jaseela99 <jaseelachittur@gmail.com>
2022-07-19 22:34:51 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,39 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function myFunction(a, b) {
+  return a + b;
+}
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+  return a === b;
+}
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+function myFunction(str) {
+  return str.substr(str.length - 3, str.length);
+}
+// Write a function that takes a value as argument
+// Return the type of the value
+function myFunction(a) {
+  return typeof a;
+}
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function myFunction(a, n) {
+  return a.charAat(n - 1);
+}
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+function myFunction(a) {
+  return a.slice(0, a.length - 3);
+}
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+function myFunction(a) {
+  return a.substr(0, a.length / 2);
+}
