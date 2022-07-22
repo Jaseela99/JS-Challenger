f166c3208fd8acb048e0355a8460813bdf246b0b
Jaseela99 <jaseelachittur@gmail.com>
2022-07-22 23:23:51 +0530

    added objects

---
+++ objects.js
@@ -0,0 +1,21 @@
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+function myFunction(obj, key) {
+  return obj[key];
+}
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+function myFunction(obj) {
+  return obj["country"];
+}
+// Write a function that takes a string as argument
+// Create an object that has a property with key 'key' and a value equal to the string
+// Return the object
+function myFunction(a) {
+  return { key: a };
+}
+// Write a function that takes a Set and a value as arguments
+// Check if the value is present in the Set
+function myFunction(set, val) {
+  return set.has(val);
+}
